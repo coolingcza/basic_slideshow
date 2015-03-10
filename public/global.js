64683cfce84ea1bce1ceb136ab012f415f207cab
Crystal Cooling <cooling.cne@gmail.com>
2015-03-09 20:25:25 -0500

    uncoupled functionality, added CSS

--- public/global.js
+++ public/global.js
@@ -1,6 +1,15 @@
+var next_s, prev_s;
+
 window.onload = function(){
   
   document.getElementById("slide").style.visibility = "hidden";
+  
+  document.getElementById("prev_s").addEventListener("click",function() {
+    change_slide(prev_s);
+  });
+  document.getElementById("next_s").addEventListener("click",function() {
+    change_slide(next_s);
+  });
   
   var start_slides = document.getElementById("start");
   start_slides.addEventListener("click", begin_slides);
@@ -14,47 +23,25 @@ window.onload = function(){
   function slide_display() {
     document.getElementById("start").style.visibility = "hidden";
     var info = JSON.parse(this.response);
-    console.log(info);
     var div_c = document.getElementById("slide");
     div_c.style.visibility = "visible";
     var children = div_c.childNodes;
-    children[1].textContent = info["title"];
-    children[2].textContent = info["body"];
+    children[5].textContent = info["title"];
+    children[7].textContent = info["body"];
     if (info["prev_s"]) {
-      prev_button = children[5];
-      prev_button.style.visibility = "visible";
-      prev_button.addEventListener("click", function() {
-        change_slide(info["prev_s"])
-      });
+      children[1].style.visibility = "visible";
+      prev_s = info["prev_s"];
     } else {
-      children[5].style.visibility = "hidden";
+      children[1].style.visibility = "hidden";
     }
     if (info["next_s"]) {
-      children[7].style.visibility = "visible";
-      children[7].addEventListener("click", function() {
-        change_slide(info["next_s"])
-      });
+      children[3].style.visibility = "visible";
+      next_s = info["next_s"];
     } else {
-      children[7].style.visibility = "hidden";
+      children[3].style.visibility = "hidden";
     }
     
-    
-    // var next_slide = document.getElementById("next");
-    // console.log(next_slide);
-    // if (next_slide.getAttribute("value")) {
-    //   next_slide.addEventListener("click",change_slide(next_slide.getAttribute("value")));
-    // }
-  
-    // var prev_slide = document.getElementById("prev");
-    // console.log(prev_slide);
-    // if (prev_slide.getAttribute("value")) {
-    //   prev_slide.addEventListener("click",change_slide(prev_slide.getAttribute("value")));
-    // }
-
-    
   }
-  
-
   
   function change_slide(v) {
     var req = new XMLHttpRequest;
